9cabcc33279eb6097c8d576051ca0b53a96031f3
Essoufi-odin <153563702+Essoufi-Odin@users.noreply.github.com>
2023-12-19 21:03:23 +0100

    made me realize how powerful each_with_index is

---
+++ ruby_path/Basic_Ruby_projects/bubble_sort.rb
@@ -0,0 +1,22 @@
+# implement bubble sort to an array
+
+def bubble_sort(array)
+#define max iterations of the array
+max_iterations = array.size - 1
+max_iterations.times do
+    array.each_with_index do |number, index|
+        
+        next_element = array[index + 1]
+        break if array[index + 1].nil? 
+        if number > next_element
+         array[index] = next_element
+         array[index + 1] = number
+        
+        end
+        
+    end
+end
+array
+ 
+end
+p bubble_sort([4,3,78,2,0,2])
