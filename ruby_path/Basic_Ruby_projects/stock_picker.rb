70d09f1d8ed21991e941ee5ac8840450001452b9
Essoufi-odin <153563702+Essoufi-Odin@users.noreply.github.com>
2023-12-19 18:37:31 +0100

    handled edge cases

--- ruby_path/Basic_Ruby_projects/stock_picker.rb
+++ ruby_path/Basic_Ruby_projects/stock_picker.rb
@@ -10,10 +10,14 @@ max_profit = 0
 possible_combinations = stocks.combination(2).to_a
 best_days = nil
 possible_combinations.each do |combination|
+    buy_index = stocks.index(combination[0])
+    sell_index = stocks.index(combination[1])
+    next if buy_index >= sell_index
+
     profit = combination[1] - combination[0]
     if max_profit < profit
         max_profit = profit
-        best_days = [stocks.index(combination[0]), stocks.index(combination[1])]
+        best_days = [buy_index, sell_index]
     end
 end
 p best_days , " for a profit of #{stocks[best_days[1]]} - #{stocks[best_days[0]]} == #{max_profit} "
